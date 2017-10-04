406fbfa6a5fc9494c732f0a70560f0e58092669b
Khanh Pham <khanh.phamdd@gmail.com>
2017-10-04 10:56:52 +0700

    Update Travis

--- config/development.js
+++ config/development.js
@@ -1,12 +1,8 @@
 module.exports = {
   db: {
-    uri: 'mongodb://mongo:27017/cloud-api-dev',
+    uri: 'mongodb://localhost:27017/cloud-api-dev',
   },
   jwt: {
     secret: '3dn0%pl*1L0NoCp116lb',
-  },
-  twilio: {
-    accountSid: 'ACb2870fdf81fca16e39fd0186fc847ad3',
-    authToken: 'eb860143601c502efc784ab2eed7c843',
   },
 }
